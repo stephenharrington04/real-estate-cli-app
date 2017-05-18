db8bf59f88a1456f2cc438d455f189a9bc7ac55b
Stephen Harrington <stephenharrington04@yahoo.com>
2017-05-18 13:54:47 +0000

    deleted most notes from Parse class

--- lib/parser.rb
+++ lib/parser.rb
@@ -7,10 +7,6 @@ class Parse
     zip_code
   end
 
-#min_price and max_price will combine into one segment
-  #if min_price = nil & max_price = nil, price = nil
-  #if min_price = nil & max price = something, price = price-na-something
-  #if min_price = something & max price = nil, price = price-something-na
   def price_parse(min_price, max_price)
     if min_price == nil && max_price == nil
       nil
@@ -21,13 +17,6 @@ class Parse
     end
   end
 
-#if bedrooms = nil, beds = nil
-#if bedrooms = studio, beds = beds-studio
-#if bedrooms = 1+, beds = beds-1
-#if bedrooms = 2+, beds = beds-2
-#if bedrooms = 3+, beds = beds-3
-#if bedrooms = 4+, beds = beds-4
-#if bedrooms = 5+, beds = beds-5
   def bedrooms_parse(bedrooms)
     beds = ""
     case bedrooms
@@ -49,12 +38,6 @@ class Parse
     beds
   end
 
-#if bathrooms = nil, baths = nil
-#if bathrooms = 1+, baths = baths-1
-#if bathrooms = 2+, baths = baths-2
-#if bathrooms = 3+, baths = baths-3
-#if bathrooms = 4+, baths = baths-4
-#if bathrooms = 5+, baths = baths-5
   def bathrooms_parse(bathrooms)
     baths = ""
     case bathrooms
@@ -74,13 +57,6 @@ class Parse
     baths
   end
 
-# if property_type = nil, type = nil
-# if property_type = single family home, type = type-single-family-home
-# if property_type = condos/townhomes/co-ops, type = type-condo-townhome-row-home-co-op
-# if property_type = mfd/mobile homes, type = type-mfd-mobile-home
-# if property_type = farms/ranches, type = type-farms-ranches
-# if property_type = land, type = type-land
-# if property_type = multi-family, type = type-multi-family-home
   def property_parse(property_type)
     type = ""
     case property_type
@@ -101,6 +77,5 @@ class Parse
     end
     type
   end
-
 
 end
