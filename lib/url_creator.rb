395b84556ac8e4f57b896e576e65a7a3071d6388
Stephen Harrington <stephenharrington04@yahoo.com>
2017-05-18 18:42:31 +0000

    added notes for the url framework, started methods

--- lib/url_creator.rb
+++ lib/url_creator.rb
@@ -1,3 +1,16 @@
 class Url_creator
-  
+
+  def initialize(parsed_inputs)
+
+  end
+
+  #basic url framework:  http://www.realtor.com/realestateandhomes-search/ZIP CODE/BEDS/BATHS/PROPERTY TYPE/PRICE RANGE
+  #http://www.realtor.com/realestateandhomes-search/  Must always be present
+  #zipcode is the only Required User_input
+  #if any of the other variables are nil, then that section will be skipped over/ommitted
+
+  def url_maker
+    web_address = "http://www.realtor.com/realestateandhomes-search/"
+  end
+
 end
