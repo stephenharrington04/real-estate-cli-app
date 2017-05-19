a515003c60ce40bc4d9c45aa4fdb8c0d3ec8bdd9
Stephen Harrington <stephenharrington04@yahoo.com>
2017-05-19 19:56:04 +0000

    started work on index_scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,6 @@ require 'nokogiri'
 
 class Scraper
 
-  attr_reader :index_url
-
-  def initialize(created_url)
-    @index_url = created_url.url
-  end
-
   #The index scraper should take in the @index_url as an argument
     #want to scrape:
       #the address
@@ -19,7 +13,22 @@ class Scraper
       #the sq ft
       #the garage size (1, 2, 3 car garage)
       #the url for the house
-  def
+  def self.index_scraper(index_url)
+    home_info = []
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    doc.css(".properties show ").css("div .page-content").css("div .container-srp").css("div .srp-body").css("div .container srp-card-wrapper").css("div .row js-sticky-container").css("section .col-lg-9 col-md-8 srp-list-column").css("div .srp-list margin-top").css("div").css("ul .srp-list-marginless list-unstyled").css("li").each do |li|
+      home_address =
+      home_price =
+      home_num_beds =
+      home_num_baths =
+      home_sqft =
+      home_garage =
+      home_url =
+      home_info << {address: home_address, price: home_price, beds: home_num_beds, baths: home_num_baths, sqft: home_sqft, garage: home_garage, url: home_url}
+    end
+    home_info
+  end
 
   #The home scraper should take in the url for the house created from the index scraper
     #want to scrape:
