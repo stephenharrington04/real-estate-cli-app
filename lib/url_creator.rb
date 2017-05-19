ad2125addff7cf41a20633eda287fe084fe77433
Stephen Harrington <stephenharrington04@yahoo.com>
2017-05-19 15:47:47 +0000

    added initialize method to url creator, working on maker method

--- lib/url_creator.rb
+++ lib/url_creator.rb
@@ -1,7 +1,7 @@
 class Url_creator
 
   def initialize(parsed_inputs)
-
+    @search_criteria = parsed_inputs.search_parameters
   end
 
   #basic url framework:  http://www.realtor.com/realestateandhomes-search/ZIP CODE/BEDS/BATHS/PROPERTY TYPE/PRICE RANGE
@@ -10,7 +10,14 @@ class Url_creator
   #if any of the other variables are nil, then that section will be skipped over/ommitted
 
   def url_maker
-    web_address = "http://www.realtor.com/realestateandhomes-search/"
+    web_address = "http://www.realtor.com/realestateandhomes-search/#{self.search_criteria[zip]}"
+    parameters = [bedrooms, bathrooms, prop_type, price_range]
+    parameters.each do |parameter|
+      self.search_criteria.each do |key, value|
+        web_address << "/#{value}" if parameter == key
+      end
+    end
+    web_address
   end
 
 end
