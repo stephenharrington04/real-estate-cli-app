271aeaffcd480bff661f34ebd35c385b6af398ac
Stephen Harrington <stephenharrington04@yahoo.com>
2017-05-23 19:19:53 +0000

    added house_scraper beginnings

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,18 +14,19 @@ class Scraper
       #the garage size (1, 2, 3 car garage)
       #the url for the house
   def self.index_scraper(index_url)
-    home_info = []
+    house_info = []
     html = open(index_url)
     doc = Nokogiri::HTML(html)
     doc.css(".properties show ").css("div.page-content").css("div.container-srp").css("div.srp-body").css("div.container srp-card-wrapper").css("div.row js-sticky-container").css("section.col-lg-9 col-md-8 srp-list-column").css("div.srp-list margin-top").css("div").css("ul.srp-list-marginless list-unstyled").css("li").each do |li|
       home_address =
-      home_price = li.css("data-price")
-      home_num_beds =
-      home_num_baths =
+      home_price = li.css("data-price").text
+      home_num_beds = li.css()
+      home_num_baths = li.css("data-baths").text
       home_sqft =
       home_garage =
-      home_url =
-      home_info << {address: home_address, price: home_price, beds: home_num_beds, baths: home_num_baths, sqft: home_sqft, garage: home_garage, url: home_url}
+      house_url =
+      house_info << {address: home_address, price: home_price, beds: home_num_beds, baths: home_num_baths, sqft: home_sqft, garage: home_garage, url: house_url}
+      binding.pry
     end
     home_info
   end
@@ -37,6 +38,20 @@ class Scraper
       #days on realtor.com
       #year built
       #description
+
+  def self.house_scraper(house_url)
+    address_info = []
+    html = open(house_url)
+    doc = Nokogiri::HTML(html)
+    doc.css #...logic...
+    house_status =
+    price_sqft =
+    days_active =
+    year_built =
+    description =
+    address_info << {status: house_status, price_per_sqft: price_sqft, days_on_market: days_active, yr_built: year_built, scription: description}
+    address_info
+  end
 
 
 
