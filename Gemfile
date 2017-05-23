4a81239bc7e9d4d3ee983c2ca07bcc379716fad5
Stephen Harrington <stephenharrington04@yahoo.com>
2017-05-23 21:26:12 +0000

    added version to gem and scrapers for price/url

--- Gemfile
+++ Gemfile
@@ -2,5 +2,5 @@
 source "https://rubygems.org"
 
 # gem "rails"
-gem 'nokogiri'
+gem 'nokogiri', '1.6.6.2'
 gem 'pry'
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,22 +13,26 @@ class Scraper
       #the sq ft
       #the garage size (1, 2, 3 car garage)
       #the url for the house
-  def self.index_scraper(index_url)
+  def index_scraper(index_url)
     house_info = []
     html = open(index_url)
     doc = Nokogiri::HTML(html)
-    doc.css(".properties show ").css("div.page-content").css("div.container-srp").css("div.srp-body").css("div.container srp-card-wrapper").css("div.row js-sticky-container").css("section.col-lg-9 col-md-8 srp-list-column").css("div.srp-list margin-top").css("div").css("ul.srp-list-marginless list-unstyled").css("li").each do |li|
+    binding.pry
+    doc.css(".properties").css(".row").css(".srp-list").css(".srp-list-marginless").css("li").each do |li|
+    first.css(".srp-item-body")
+      #.css("div.page-content").css("div.container-srp").css("div.srp-body").css("div.container srp-card-wrapper").css("div.row js-sticky-container").css("section.col-lg-9 col-md-8 srp-list-column").css("div.srp-list margin-top").css("div").css("ul.srp-list-marginless list-unstyled").css("li").each do |li|
+      house_url = "http://www.realtor.com"
       home_address =
-      home_price = li.css("data-price").text
+      home_price = li.css(".srp-item-body").css(".srp-item-price").css(".data-price-display").text
       home_num_beds = li.css()
       home_num_baths = li.css("data-baths").text
       home_sqft =
       home_garage =
-      house_url =
+      house_url << li.css(".srp-item-body").css("a").attributes("href").value
       house_info << {address: home_address, price: home_price, beds: home_num_beds, baths: home_num_baths, sqft: home_sqft, garage: home_garage, url: house_url}
-      binding.pry
+
     end
-    home_info
+    house_info
   end
 
   #The home scraper should take in the url for the house created from the index scraper
@@ -38,6 +42,7 @@ class Scraper
       #days on realtor.com
       #year built
       #description
+
 
   def self.house_scraper(house_url)
     address_info = []
@@ -52,8 +57,7 @@ class Scraper
     address_info << {status: house_status, price_per_sqft: price_sqft, days_on_market: days_active, yr_built: year_built, scription: description}
     address_info
   end
-
-
-
-
 end
+
+m = Scraper.new
+m.index_scraper("http://www.realtor.com/realestateandhomes-search/67037/beds-1/type-single-family-home/price-250000-400000")
