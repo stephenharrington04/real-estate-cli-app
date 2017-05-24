282cda7bc9079e5a300e9d5a4ee42d52296116b7
Stephen Harrington <stephenharrington04@yahoo.com>
2017-05-24 01:54:49 +0000

    working on fixing errors in index_scraper method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,28 +13,27 @@ class Scraper
       #the url for the house
   def index_scraper(index_url)
     house_info = []
-    html = open(index_url)
-    doc = Nokogiri::HTML(html)
-    binding.pry
-    #  doc.css(".properties").css(".row").css(".srp-list").css(".srp-list-marginless").css("li").first.css(".srp-item-body").css(".srp-item-property-meta").css("ul").css("li").css("span.data-value")
+    doc = Nokogiri::HTML(open(index_url))
+    #doc.css(".properties").css(".row").css(".srp-list").css(".srp-list-marginless").css("li").first.css(".srp-item-body").css(".srp-item-details").css("a").first.attr("href")
     doc.css(".properties").css(".row").css(".srp-list").css(".srp-list-marginless").css("li").each do |li|
-    first.css(".srp-item-body")
       house_url = "http://www.realtor.com"
       home_address = []
       home_address << li.css(".srp-item-body").css(".srp-item-address").css(".listing-street-address").text
       home_address << li.css(".srp-item-body").css(".srp-item-address").css(".listing-city").text
       home_address << li.css(".srp-item-body").css(".srp-item-address").css(".listing-region").text
       home_address << li.css(".srp-item-body").css(".srp-item-address").css(".listing-postal").text
-      home_address.join
+      home_address.join(" ")
       home_price = li.css(".srp-item-body").css(".srp-item-price").css(".data-price-display").text
       home_num_beds = li.css(".srp-item-body").css(".srp-item-property-meta").css("ul").css("li").css("span.data-value.meta-beds").text
       home_num_baths = li.css(".srp-item-body").css(".srp-item-property-meta").css("ul").css("li")[1].css("span.data-value").text
-      house_url << li.css(".srp-item-body").css("a").attribute("href").value
+      house_url << li.css(".srp-item-body").css(".srp-item-details").css("a").first.attr("href")
       house_info << {address: home_address, price: home_price, beds: home_num_beds, baths: home_num_baths, url: house_url}
+      binding.pry
+
     end
     house_info
-  end
 
+  end
   #The home scraper should take in the url for the house created from the index scraper
     #want to scrape:
       #status
@@ -44,20 +43,19 @@ class Scraper
       #description
 
 
-  def self.house_scraper(house_url)
-    address_info = []
-    html = open(house_url)
-    doc = Nokogiri::HTML(html)
-    doc.css #...logic...
-    house_status =
-    price_sqft =
-    days_active =
-    year_built =
-    description =
-    address_info << {status: house_status, price_per_sqft: price_sqft, days_on_market: days_active, yr_built: year_built, scription: description}
-    address_info
-  end
+  #def self.house_scraper(house_url)
+  #  address_info = []
+  #  html = open(house_url)
+  #  doc = Nokogiri::HTML(html)
+#    doc.css #...logic...
+#    house_status =
+  #  price_sqft =
+#    days_active =
+#    year_built =
+#    description =
+#    address_info << {status: house_status, price_per_sqft: price_sqft, days_on_market: days_active, yr_built: year_built, scription: description}
+#    address_info
+#  end
 end
-
 m = Scraper.new
 m.index_scraper("http://www.realtor.com/realestateandhomes-search/67037/beds-1/type-single-family-home/price-250000-400000")
