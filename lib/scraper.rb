aaf6d3be0db65b1f6747b16807a9517f9eed7ef3
Stephen Harrington <stephenharrington04@yahoo.com>
2017-05-24 15:49:12 +0000

    changed loop, fixed addresss issues, working url/bath scraping isues

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,25 +14,21 @@ class Scraper
     house_info = []
     doc = Nokogiri::HTML(open(index_url))
     #doc.css(".properties").css(".row").css(".srp-list").css(".srp-list-marginless").css("li").first.css(".srp-item-body").css(".srp-item-details").css("a.srp-card-anchor-overlay").attribute("href").value
-    doc.css(".properties").css(".row").css(".srp-list").css(".srp-list-marginless").each do |li|
+    doc.css(".properties").css(".row").css(".srp-list").css(".srp-list-marginless").css("li").each do |listing|
       house_url = "http://www.realtor.com"
-      home_address = []
-      home_address << li.css(".srp-item-body").css(".srp-item-address").css(".listing-street-address").text
-      home_address << li.css(".srp-item-body").css(".srp-item-address").css(".listing-city").text
-      home_address << li.css(".srp-item-body").css(".srp-item-address").css(".listing-region").text
-      home_address << li.css(".srp-item-body").css(".srp-item-address").css(".listing-postal").text
-      home_address.join(" ")
-      home_price = li.css(".srp-item-body").css(".srp-item-price").css(".data-price-display").text
-      home_num_beds = li.css(".srp-item-body").css(".srp-item-property-meta").css("ul").css("li").css("span.data-value.meta-beds").text
-      home_num_baths = li.css(".srp-item-body").css(".srp-item-property-meta").css("ul").css("li")[1].css("span.data-value").text
-      # doc.css(".properties").css(".row").css(".srp-list").css(".srp-list-marginless").css("li").css(".js-record-user-activity.js-navigate-to.srp-item").attr("data-url").value
-      house_url << li.css(".js-record-user-activity.js-navigate-to.srp-item").attr("data-url").value
-      #css(".srp-item-body").css(".srp-item-details").css("a.srp-card-anchor-overlay").attribute("href").value
-      house_info << {address: home_address, price: home_price, beds: home_num_beds, baths: home_num_baths, url: house_url}
-
+      street_address = listing.css(".srp-item-body").css(".srp-item-address").css(".listing-street-address").text
+      city_address = listing.css(".srp-item-body").css(".srp-item-address").css(".listing-city").text
+      state_address = listing.css(".srp-item-body").css(".srp-item-address").css(".listing-region").text
+      postal_address = listing.css(".srp-item-body").css(".srp-item-address").css(".listing-postal").text
+      home_address = "#{street_address} #{city_address}, #{state_address}, #{postal_address}"
+      home_price = listing.css(".srp-item-body").css(".srp-item-price").css(".data-price-display").text
+      home_num_beds = listing.css(".srp-item-body").css(".srp-item-property-meta").css("ul").css("li").css("span.data-value.meta-beds").text
+      #home_num_baths = listing.css(".srp-item-body").css(".srp-item-property-meta").css("ul").css("li")[1].css("span.data-value").text
+      #house_url << li.css(".js-record-user-activity.js-navigate-to.srp-item").attr("data-url").value
+      #house_url << listing.css(".srp-item-body").css(".srp-item-details").css("a.srp-card-anchor-overlay").attribute("href").value
+      house_info << {address: home_address, price: home_price, beds: home_num_beds} if home_address != " , , "
     end
     house_info
-
   end
   #The home scraper should take in the url for the house created from the index scraper
     #want to scrape:
@@ -57,5 +53,6 @@ class Scraper
 #    address_info
 #  end
 end
+binding.pry
 m = Scraper.new
 m.index_scraper("http://www.realtor.com/realestateandhomes-search/67037/beds-1/type-single-family-home/price-250000-400000")
