66ba8fa9304c7dc195d6bcfbbd74fb3728e4a7ea
Stephen Harrington <stephenharrington04@yahoo.com>
2017-05-30 22:03:38 +0000

    combined basic_info scraper into listing_scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -53,39 +53,31 @@ class Scraper
 
 ################## INDIVIDUAL HOUSE SCRAPER FUNCTIONS ############################################
 
-  def basic_info_scraper(listing_url)
-    data = {}
-    info = []
-    doc = Nokogiri::HTML(open(listing_url))
-    main = doc.css(".page-content").css(".container-ldp").css(".container").css(".row-wrapper-detail").css(".row").css(".col-lg-9").css(".listing-header").css(".listing-header-main").css(".row").css(".col-sm-7").css(".pull-left")
-    sections = main.css(".ldp-header-meta").css("ul").css("li")
-    sections.css("span").each do |item|
-      info << item.text
-    end
-    data[:address] = main.css(".ldp-header-address-wrapper").first.children.css("div").first.attr("content")
-    data[:beds] = info[0]
-    data[:baths] = info[1]
-    data[:sqft] = info[2]
-    data[:acres] = info[3]
-    binding.pry
-  end
-
   def listing_scraper(listing_url)
     listing_info = {}
     facts = []
     doc = Nokogiri::HTML(open(listing_url))
-    basic_noko = doc.css(".page-content").css(".container-ldp").css(".container").css(".row-wrapper-detail").css(".row").css(".col-lg-9").css(".listing-section")
-    detailed_noko = basic_noko.css(".listing-subsection")
+    basic_noko = doc.css(".page-content").css(".container-ldp").css(".container").css(".row-wrapper-detail").css(".row").css(".col-lg-9")
+    detailed_noko = basic_noko.css(".listing-section").css(".listing-subsection")
     detailed_noko.css(".ldp-detail-key-facts").css("ul").css("li").css(".key-fact-data").each do |fact|
       facts << fact.text
     end
-    listing_info[:address] = basic_noko.css("h2").css("span.visible-lg-inline").text.gsub("for ", "")
+    basic_noko.css(".listing-header").css(".listing-header-main").css(".row").css(".col-sm-7").css(".pull-left").css(".ldp-header-meta").css("ul").css("li").css("span").each do |item|
+      facts << item.text
+    end
+    listing_info[:address] = basic_noko.css(".listing-header").css(".listing-header-main").css(".row").css(".col-sm-7").css(".pull-left").css(".ldp-header-address-wrapper").first.children.css("div").first.attr("content")
+    listing_info[:beds] = facts[5]
+    listing_info[:baths] = facts[6]
+    listing_info[:sqft] = facts[7]
+    listing_info[:acres] = facts[8]
     listing_info[:status] = facts[0]
     listing_info[:price_per_sqft] = facts[1]
     listing_info[:days_on_market] = facts[2]
     listing_info[:year_built] = facts [3]
     listing_info[:property_type] = facts [4]
     listing_info[:description] = detailed_noko.css(".margin-top-lg").css("p").first.text
+    binding.pry
+
     listing_info
   end
 
@@ -94,4 +86,4 @@ end
 m = Scraper.new
 #m.index_scraper("http://www.realtor.com/realestateandhomes-search/67037/beds-1/type-single-family-home/price-250000-400000")
 #m.url_scraper("http://www.realtor.com/realestateandhomes-search/67037/beds-1/type-single-family-home/price-250000-400000")
-m.basic_info_scraper("http://www.realtor.com/realestateandhomes-detail/929-E-Lost-Hills-Ct_Derby_KS_67037_M74395-08416")
+m.listing_scraper("http://www.realtor.com/realestateandhomes-detail/2400-N-Sawgrass-Ct_Derby_KS_67037_M80297-52062")
