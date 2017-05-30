341a2ba7464abfb652b45719e893cb257f706fd8
Stephen Harrington <stephenharrington04@yahoo.com>
2017-05-30 20:27:34 +0000

    deleted notes, changed some variable names in listing_scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,13 +4,7 @@ require 'nokogiri'
 
 class Scraper
 ################## SEARCH QUERY SCRAPER FUNCTIONS ########################
-  #The index scraper should take in an index_url as an argument
-    #want to scrape:
-      #the address
-      #the price
-      #the beds
-      #the baths
-      #the url for the house
+
   def url_scraper(url)
     doc = Nokogiri::HTML(open(url))
     urls = []
@@ -57,48 +51,24 @@ class Scraper
     house_info
   end
 
-
 ################## INDIVIDUAL HOUSE SCRAPER FUNCTIONS ############################################
-  #The home scraper should take in the url for the house created from the index scraper
-    #want to scrape:
-      #address
-      #status
-      #price/sq ft
-      #days on realtor.com
-      #year built
-      #description
 
-  def house_scraper(url)
-    address_info = []
-    doc = Nokogiri::HTML(open(url))
-    website_basic = doc.css(".page-content").css(".container-ldp").css(".container").css(".row-wrapper-detail").css(".row").css(".col-lg-9").css(".listing-section")
-    website_details = website_basic.css(".listing-subsection").css(".ldp-detail-key-facts").css("ul")
-    address_info << website_basic.css("h2").css("span.visible-lg-inline").text.gsub("for ", "")
-    website_details.css("li").css(".key-fact-data").collect do |fact|
-      address_info << fact.text
+  def listing_scraper(listing_url)
+    listing_info = []
+    doc = Nokogiri::HTML(open(listing_url))
+    basic_noko = doc.css(".page-content").css(".container-ldp").css(".container").css(".row-wrapper-detail").css(".row").css(".col-lg-9").css(".listing-section")
+    detailed_noko = basic_noko.css(".listing-subsection")
+    listing_info << basic_noko.css("h2").css("span.visible-lg-inline").text.gsub("for ", "")
+    detailed_noko.css(".ldp-detail-key-facts").css("ul").css("li").css(".key-fact-data").collect do |fact|
+      listing_info << fact.text
     end
-    address_info << website_basic.css(".listing-subsection").css(".margin-top-lg").css("p").first.text
-    binding.pry
+    listing_info << detailed_noko.css(".margin-top-lg").css("p").first.text
   end
 
-
-
-  #def house_scraper(house_url)
-  #  address_info = []
-  #  html = open(house_url)
-  #  doc = Nokogiri::HTML(html)
-#    doc.css #...logic...
-#    house_status =
-  #  price_sqft =
-#    days_active =
-#    year_built =
-#    description =
-#    address_info << {status: house_status, price_per_sqft: price_sqft, days_on_market: days_active, yr_built: year_built, scription: description}
-#    address_info
-#  end
 end
 
 m = Scraper.new
 #m.index_scraper("http://www.realtor.com/realestateandhomes-search/67037/beds-1/type-single-family-home/price-250000-400000")
 #m.url_scraper("http://www.realtor.com/realestateandhomes-search/67037/beds-1/type-single-family-home/price-250000-400000")
-m.house_scraper("http://www.realtor.com/realestateandhomes-detail/929-E-Lost-Hills-Ct_Derby_KS_67037_M74395-08416")
+m.listing_scraper("http://www.realtor.com/realestateandhomes-detail/929-E-Lost-Hills-Ct_Derby_KS_67037_M74395-08416")
+binding.pry
