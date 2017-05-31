5f62db09c27edeb36ee1dfd72080330e61c71bd1
Stephen Harrington <stephenharrington04@yahoo.com>
2017-05-31 18:23:16 +0000

    added attr_accessors to listing class

--- lib/listing.rb
+++ lib/listing.rb
@@ -1,6 +1,8 @@
 
 class Listing
 
+  attr_accessor address:, price:, beds:, baths:, property_type:, house_url:, sqft:, acres:, status:, price_per_sqft:, days_on_market:, year_built:, description:
+
   def intialize(listings_hash)
     listings_hash.each {|key, value| self.send("#{key}=", value)}
     @@all << self
@@ -9,7 +11,5 @@ class Listing
   def self.all
     @@all
   end
-
-
 
 end
