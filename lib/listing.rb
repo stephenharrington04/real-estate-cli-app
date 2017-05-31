984c0b5a4d0ead57d07d0cb5c29ca3f9566a675f
Stephen Harrington <stephenharrington04@yahoo.com>
2017-05-31 19:09:18 +0000

    added @all constant to listing class

--- lib/listing.rb
+++ lib/listing.rb
@@ -3,6 +3,8 @@ class Listing
 
   attr_accessor :address, :price, :beds, :baths, :property_type, :house_url, :sqft, :acres, :status, :price_per_sqft, :days_on_market, :year_built, :description
 
+  @@all =[]
+
   def initialize(listing_hash)
     listing_hash.each {|key, value| self.send("#{key}=", value)}
     @@all << self
