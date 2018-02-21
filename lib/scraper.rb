341af5cc2ea1cacc10209c027eea3ab76b357ae0
Stephen Harrington <stephenharrington04@yahoo.com>
2018-02-21 21:54:45 +0000

    about cleaned up scraper class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -49,35 +49,43 @@ class Scraper
 
   def listing_scraper(listing_url)
     listing_info = {}
-    facts = []
     prop_details = []
+
     doc = Nokogiri::HTML(open(listing_url))
-    basic_noko = doc.css(".page-content").css(".container-ldp").css(".container").css(".row-wrapper-detail").css(".row").css(".col-lg-9")
-    detailed_noko = basic_noko.css(".listing-section").css(".listing-subsection")
-    detailed_noko.css(".ldp-detail-key-facts").css("ul").css("li").css(".key-fact-data").each do |fact|
-      facts << fact.text
-    end
+
     doc.css(".key-fact-data").each do |detail|
       prop_details << detail.text
     end
-    basic_noko.css(".listing-header").css(".listing-header-main").css(".row").css(".col-sm-7").css(".pull-left").css(".ldp-header-meta").css("ul").css("li").css("span").each do |item|
-      facts << item.text
-    end
+
     listing_info[:address] = doc.css(".ldp-header-address-wrapper").first.children.css("div").first.attr("content")
     listing_info[:price] = doc.css(".display-inline").css("span")[2].text.scan(/\d|\$|,/).join
-    listing_info[:beds] = facts[5]
-    listing_info[:baths] = facts[6]
-    listing_info[:sqft] = facts[7]
-    listing_info[:acres] = facts[8]
+    listing_info[:beds] = doc.css(".ldp-header-meta").css("ul").css("li[data-label='property-meta-beds']").css("span").text
+    listing_info[:baths] = baths(doc.css(".ldp-header-meta").css("ul").css("li[data-label='property-meta-baths']").css("span").collect {|span| span.text})
+    listing_info[:baths] ||= doc.css(".ldp-header-meta").css("ul").css("li[data-label='property-meta-bath']").css("span").text
+    listing_info[:sqft] = doc.css(".ldp-header-meta").css("ul").css("li[data-label='property-meta-sqft']").css("span").text
+    listing_info[:acres] = doc.css(".ldp-header-meta").css("ul").css("li[data-label='property-meta-lotsize']").css("span").text
     listing_info[:status] = prop_details[0].strip
     listing_info[:price_per_sqft] = prop_details[1]
     listing_info[:days_on_market] = prop_details[2]
     listing_info[:property_type] = prop_details[3]
     listing_info[:year_built] = prop_details[4]
     listing_info[:style] = prop_details[5].strip
-    listing_info[:description] = detailed_noko.css(".margin-top-lg").css("p").first.text
+    listing_info[:description] = doc.css(".margin-top-lg").css("p").first.text
     listing_info
     binding.pry
+  end
+
+  def baths(bath_array)
+    case bath_array.size
+    when 0 || nil
+      nil
+    when 1
+      bath_array.push(" Full").join
+    when 2
+      bath_array.insert(1, " Full, ")
+      bath_array << " Half"
+      bath_array.join
+    end
   end
 
 
@@ -85,4 +93,4 @@ class Scraper
 end
 
 m = Scraper.new
-m.listing_scraper("https://www.realtor.com/realestateandhomes-detail/1433-Ashfield-Gln_O-Fallon_IL_62269_M82829-38742")
+m.listing_scraper("https://www.realtor.com/realestateandhomes-detail/1401-Amberleaf-Ct_O-Fallon_IL_62269_M75152-69436")
