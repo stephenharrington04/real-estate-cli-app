a0aa96ebe509211b7aff2d80216be0344e214ebc
Stephen Harrington <stephenharrington04@yahoo.com>
2018-02-26 01:44:18 +0000

    reworked url maker method

--- lib/url_creator.rb
+++ lib/url_creator.rb
@@ -7,24 +7,15 @@ class Url_creator
     @url = self.url_maker
   end
 
-  parsed_parameters = {
-    parsed_zip_code: search_parameters.zipcode,
-    parsed_bedrooms: bedrooms_parse(search_parameters),
-    parsed_bathrooms: bathrooms_parse(search_parameters),
-    parsed_property_type: property_type_parse(search_parameters),
-    parsed_price_range: price_parse(search_parameters)
-  }
-  end
-
   def url_maker
     web_address = "https://www.realtor.com/realestateandhomes-search/#{self.parsed_parameters_hash[:parsed_zip_code]}"
-    self.parsed_parameters_hash.each do |key, value|
-      web_address << "#{value}"
+    parameters = ["parsed_bedrooms", "parsed_bathrooms", "parsed_property_type", "parsed_price_range"]
+    parameters.each do |parameter|
+      self.search_criteria.each do |key, value|
+        web_address << "/#{value}" if parameter == key.to_s
+      end
     end
-    web_address << parsed_parameters.parsed_bedrooms if parsed_parameters.bedrooms
-    web_address << parsed_parameters.parsed_bathrooms if parsed_parameters.bathrooms
-    web_address << parsed_parameters.parsed_property_type if parsed_parameters.parsed_property_type
-    web_address << parsed_parameters.parsed_price_range if parsed_parameters.parsed_price_range
+    web_address
   end
 
 end
