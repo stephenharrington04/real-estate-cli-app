299941341a2499028ceb695dbcb7fd1f66fa95f9
Stephen Harrington <stephenharrington04@yahoo.com>
2018-02-26 16:12:50 +0000

    modified CLI to account for URL maker change

--- lib/url_creator.rb
+++ lib/url_creator.rb
@@ -1,12 +1,4 @@
 class Url_creator
-
-  #attr_reader :formatted_parameters_hash, :url
-#
-  #def initialize(formatted_parameters_hash)
-  #  @formatted_parameters_hash = formatted_parameters_hash
-  #  @url = self.url_maker
-  #end
-
   def self.url_maker(formatted_parameters_hash)
     web_address = "https://www.realtor.com/realestateandhomes-search/#{formatted_parameters_hash[:formatted_zip_code]}"
     parameters = ["formatted_bedrooms", "formatted_bathrooms", "formatted_property_type", "formatted_price_range"]
@@ -17,5 +9,4 @@ class Url_creator
     end
     web_address
   end
-
 end
