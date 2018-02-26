93cbcb0bebe960771328bbe70814a4475de909c0
github username <github email address>
2018-02-26 16:11:37 -0600

    added stylizing to the welcome message

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,8 @@
 require_relative '../config/environment'
 
 start_app = CommandLineInterface.new
-puts "Welcome to the Real Estate Application!"
+puts ""
+puts "--------------------------------------------".colorize(:blue)
+puts "|  WELCOME TO THE REAL ESTATE APPLICATION  |".colorize(:blue)
+puts "--------------------------------------------".colorize(:blue)
 start_app.run
